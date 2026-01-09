16d7f2ab30cd573afb5009924587d0a171daaa19
Anastasiia Beshleha <anastasiiabeshleha@gmail.com>
2026-01-08 22:57:03 -0400

    Sequence practice

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,12 @@
 package mate.academy
 
-fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+const val MAGIC = 25
 
+fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+    return numbers
+        .map { i ->
+            if (i % 2 == 0) i / 2 else i * 2
+        }
+        .filter { it > MAGIC }
+        .toMutableList()
 }
